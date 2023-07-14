1b7b68327dd6d0e7eec90714ad4fdf274ed54247
SOUROV <sourovsarker005@gmail.com>
2023-07-15 00:20:47 +0600

    Django 404 Template

--- my_tennis_club/my_tennis_club/settings.py
+++ my_tennis_club/my_tennis_club/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-lx(7)ku%y*=a59x80=gr^8n%r2ku8$$+=x=%8ce9-0143dk7-n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
